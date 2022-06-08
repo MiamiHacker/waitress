3fe4d655eb1d10eb157db0e2cd6f6205d5299e2b
MiamiHacker <105590216+MiamiHacker@users.noreply.github.com>
2022-06-08 05:38:25 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "<h1>Hello World!</h1>"
+
+if __name__ == "__main__":
+  serve(app, host="0.0.0.0", port=8080)
+  
